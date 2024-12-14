65a072b4abc0cf40302fa5cb6ff3bba276acf238
Umaid <ahmedumaid707@gmail.com>
2024-12-15 00:13:35 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,8 +14,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')
+                        InlineKeyboardButton('• ʜᴏᴍᴇ', callback_data='start'),
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ •", callback_data='close')
                     ]
                 ]
             )
@@ -26,8 +26,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                     InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+                    [InlineKeyboardButton('• ʜᴏᴍᴇ', callback_data='start'),
+                     InlineKeyboardButton('ᴄʟᴏꜱᴇ •', callback_data='close')]
                 ]
             )
         )
@@ -36,12 +36,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=START_MSG.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("• ʜᴇʟᴘ", callback_data='help'),
-                 InlineKeyboardButton("ᴀʙᴏᴜᴛ •", callback_data='about')],
-                [InlineKeyboardButton('• ᴀɴɪᴍᴇ', url='https://t.me/anime_cruise_netflix'),
-                 InlineKeyboardButton('ᴡᴇʙsᴇʀɪᴇs •', url='https://t.me/webseries_flix')],
-                [InlineKeyboardButton("• ᴄʟᴏꜱᴇ •", callback_data='close')]
-            ])
+                    [InlineKeyboardButton("• ғᴏʀ ᴍᴏʀᴇ •", url='https://t.me/anime_sub_society')],
+                    [InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data='about'),
+                     InlineKeyboardButton("ʜᴇʟᴘ •", url='https://t.me/ahss_help_zone')],
+                    [InlineKeyboardButton("• ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ •", url='https://t.me/society_network')],
+                ])
         )
     
     elif data == "close":
